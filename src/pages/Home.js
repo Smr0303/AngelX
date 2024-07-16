257c105f517b775d298213c2dc6a9a8868117aa8
Smr0303 <samarmohite7@gmail.com>
2024-07-17 00:45:52 +0530

    Completed

---
+++ src/pages/Home.js
@@ -0,0 +1,56 @@
+import styles from "styles/Home.module.scss";
+import Card from "components/Card";
+import Title from "components/Title";
+import useMakeRequest from "hooks/useMakeRequest";
+import { useState, useEffect } from "react";
+
+const Home = () => {
+  const { data, error } = useMakeRequest("https://fakestoreapi.com/products/");
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    if (data) {
+      setProducts(data);
+    }
+  }, [data]);
+
+  const handleAddToCart = (productId) => {
+
+    setProducts(products.filter(product => product.id !== productId));
+  };
+
+  if (!data) {
+    if (error) {
+      return (
+        <div style={{ width: "100%", display: "flex", justifyContent: "center", marginTop: "30px" }}>
+          <Title txt={error} size={25} transform="uppercase" />
+        </div>
+      );
+    } else {
+      return (
+        <div style={{ width: "100%", display: "flex", justifyContent: "center", marginTop: "30px" }}>
+          <Title txt="Loading..." size={25} transform="uppercase" />
+        </div>
+      );
+    }
+  } else {
+    return (
+      <section className={styles.home}>
+        <div className={styles.container}>
+          <div className={styles.row}>
+            <div className={styles.title}>
+              <Title txt="all products" color="#171717" size={22} transform="uppercase" />
+            </div>
+          </div>
+          <div className={styles.row}>
+            {products.map((product, key) => (
+              <Card product={product} key={key} onAddToCart={() => handleAddToCart(product.id)} />
+            ))}
+          </div>
+        </div>
+      </section>
+    );
+  }
+};
+
+export default Home;
